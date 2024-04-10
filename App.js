3eccacaf9c4405da66a615a4b9ca5c2bff794cb9
Sinan <sinan_ceviker@yahoo.com>
2024-04-10 05:31:59 +0200

    implemented cached storage of times

--- App.js
+++ App.js
@@ -1,16 +1,93 @@
-import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import React, { useEffect } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import PrayerTimesScreen from './screens/PrayerTimesScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  useEffect(() => {
+    const getLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        return null;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      return location;
+    };
+
+    const setCityAndCountry = async (location) => {
+      const { latitude, longitude } = location.coords;
+      const reverseGeocode = await Location.reverseGeocodeAsync({ 
+        latitude: latitude, 
+        longitude: longitude
+      });
+      if (reverseGeocode) {
+        const city = reverseGeocode[0].city;
+        const country = reverseGeocode[0].country;
+        await AsyncStorage.setItem('city', city);
+        await AsyncStorage.setItem('country', country);
+      }
+    };
+
+    const checkStorage = async () => {
+      const storedCity = await AsyncStorage.getItem('city');
+      const storedCountry = await AsyncStorage.getItem('country');
+      if (storedCity && storedCountry) {
+        return;
+      }
+      const location = await getLocation();
+      if (location) {
+        await setCityAndCountry(location);
+      } else {
+        await AsyncStorage.setItem('city', 'Groningen');
+        await AsyncStorage.setItem('country', 'Netherlands');
+      }
+    };
+
+    const checkAladhan = async () => {
+      if (await AsyncStorage.getItem('Aladhan')) {
+        return;
+      }
+      const city = await AsyncStorage.getItem('city');
+      const country = await AsyncStorage.getItem('country');
+      const date = new Date();
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const response = await fetch(`http://api.aladhan.com/v1/calendarByCity/${year}/${month}?city=${city}&country=${country}&method=2`);
+      while (!response.ok) {
+        response = await fetch(`http://api.aladhan.com/v1/calendarByCity/${year}/${month}?city=${city}&country=${country}&method=2`);
+      }
+      const data = await response.json();
+      if (data) {
+        const day = date.getDate() - 1;
+        await AsyncStorage.setItem('Aladhan', JSON.stringify(data.data));
+      }
+    };
+
+    checkStorage();
+    checkAladhan();
+  }, []);
+
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="PrayerTimes">
-        <Drawer.Screen name="PrayerTimes" component={PrayerTimesScreen} />
-      </Drawer.Navigator>
+      <Tab.Navigator 
+        initialRouteName='PrayerTimes'
+        screenOptions={{
+          tabBarActiveTintColor: '#0b3d91',
+          tabBarStyle: { backgroundColor: '#0b3d91' },
+        }}
+      >
+        <Tab.Screen 
+          name='PrayerTimes' 
+          component={PrayerTimesScreen} 
+          options={{
+            tabBarLabel: 'Prayer Times',
+            tabBarActiveTintColor: 'white',
+          }}  
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- screens/PrayerTimesScreen.js
+++ screens/PrayerTimesScreen.js
@@ -2,85 +2,57 @@ import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function PrayerTimesScreen() {
   const [city, setCity] = useState('');
-  const [country, setCountry] = useState('');
   const [prayerTimes, setPrayerTimes] = useState(null);
   const [hijriDate, setHijriDate] = useState('');
-  const [errorMsg, setErrorMsg] = useState(null);
-
-  const getLocation = async () => {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      setErrorMsg('Permission to access location was denied');
-      return null;
-    }
-
-    let location = await Location.getLastKnownPositionAsync({});
-    return location;
-  }
-
-  const setCityAndCountry = async (location) => {
-    const { latitude, longitude } = location.coords;
-    console.log(latitude, longitude);
-    const reverseGeocode = await Location.reverseGeocodeAsync({ latitude, longitude });
-    if (reverseGeocode) {
-      setCity(reverseGeocode[0].city);
-      setCountry(reverseGeocode[0].country);
-    } else {
-      setErrorMsg('No known location found');
-    }
-  }
-
-  const setPrayingTimes = async () => {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-
-    const response = await fetch(`http://api.aladhan.com/v1/calendarByCity/${year}/${month}?city=${city}&country=${country}&method=2`);
-    const data = await response.json();
-    if (data) {
-      const day = date.getDate() - 1;
-      console.log(data)
-      setPrayerTimes(data.data[day].timings);
-      console.log(data.data[day].timings);
-      await setupHijriDate(data.data[day].date.hijri);
-    } else {
-      setErrorMsg('Error getting prayer times.');
-      console.log('error')
-    }
-  };
-
-  const setupHijriDate = async (data) => {
-    const day = data.day;
-    const month = data.month.en;
-    const year = data.year;
-    setHijriDate(`${day} ${month} ${year}`);
-  }
 
   useEffect(() => {
-    (async () => {
-      const location = await getLocation();
-      if (location) {
-        await Promise.all([setCityAndCountry(location)]);
-        await setPrayingTimes();
-      }
-    })();
+    const fetchInfo = async () => {
+      const city = await AsyncStorage.getItem('city');
+      setCity(city);
+      
+      const data = JSON.parse(await AsyncStorage.getItem('Aladhan'));
+      const date = new Date();
+      const day = date.getDate() - 1;
+      
+      setPrayerTimes({
+        "Imsak": data[day].timings.Imsak.split(' ')[0],
+        "Sunrise": data[day].timings.Sunrise.split(' ')[0],
+        "Dhuhr": data[day].timings.Dhuhr.split(' ')[0],
+        "Asr": data[day].timings.Asr.split(' ')[0],
+        "Maghrib": data[day].timings.Maghrib.split(' ')[0],
+        "Isha": data[day].timings.Isha.split(' ')[0],
+      });
+
+      const hijriInfo = data[day].date.hijri;
+      setHijriDate(`${hijriInfo.day} ${hijriInfo.month.en} ${hijriInfo.year}`);
+
+    };
+
+    fetchInfo();
   }, []);
 
   return (
     <View style={styles.container}>
       <Text>Mosque Al-Rahma</Text>
-      {city && <Text>City: {city}</Text>}
-      {country && <Text>Country: {country}</Text>}
-      {hijriDate && <Text>Hijri Date: {hijriDate}</Text>}
-      {prayerTimes && Object.keys(prayerTimes).map((key, index) => (
-        <Text key={index}>{key}: {prayerTimes[key]}</Text>
-      ))}
+      {city ? <Text>{city}</Text> : null}
+      {prayerTimes ? (
+        <View>
+          <Text>Imsak: {prayerTimes.Imsak}</Text>
+          <Text>Sunrise: {prayerTimes.Sunrise}</Text>
+          <Text>Dhuhr: {prayerTimes.Dhuhr}</Text>
+          <Text>Asr: {prayerTimes.Asr}</Text>
+          <Text>Maghrib: {prayerTimes.Maghrib}</Text>
+          <Text>Isha: {prayerTimes.Isha}</Text>
+        </View>
+      ) : null}
+      {hijriDate ? <Text>{hijriDate}</Text> : null}
       <StatusBar style="auto" />
     </View>
-  );
+);
 }
 
 const styles = StyleSheet.create({
