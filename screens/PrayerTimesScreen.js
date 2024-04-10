df9f7c24a6e5201d13ae07a380da76463e7796d6
Sinan <sinan_ceviker@yahoo.com>
2024-04-10 12:46:37 +0200

    setup remaining time

--- screens/PrayerTimesScreen.js
+++ screens/PrayerTimesScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location';
@@ -7,7 +7,51 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export default function PrayerTimesScreen() {
   const [city, setCity] = useState('');
   const [prayerTimes, setPrayerTimes] = useState(null);
+  const [nextPrayer, setNextPrayer] = useState('');
+  const [timeToNextPrayer, setTimeToNextPrayer] = useState('');
   const [hijriDate, setHijriDate] = useState('');
+  const countdownRef = useRef(null);
+
+  const getNextPrayer = () => {
+    const now = new Date();
+    const currentSeconds = now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+
+    const prayerSeconds = Object.entries(prayerTimes).map(([prayer, time]) => {
+      const [hours, minutes] = time.split(':').map(Number);
+      return [prayer, hours * 3600 + minutes * 60];
+    });
+
+    const next = prayerSeconds.find(([_, prayerTime]) => prayerTime > currentSeconds);
+
+    if (next) {
+      setNextPrayer(next[0]);
+      startCountdown(next[1] - currentSeconds);
+    } else {
+      setNextPrayer('Imsak');
+      startCountdown(prayerSeconds[0][1] + 24 * 3600 - currentSeconds);
+    }
+  }
+
+  const startCountdown = (timeToNextPrayer) => {
+    let totalSeconds = timeToNextPrayer;
+
+    const countdown = setInterval(() => {
+      totalSeconds--;
+
+      const hours = Math.floor(totalSeconds / 3600);
+      const minutes = Math.floor((totalSeconds % 3600) / 60);
+      const seconds = (totalSeconds % 60);
+
+      const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
+      setTimeToNextPrayer(`${formattedTime}`);
+
+      if (totalSeconds === 0) {
+        clearInterval(countdown);
+      }
+    }, 1000);
+
+    return countdownRef.current;
+  }
 
   useEffect(() => {
     const fetchInfo = async () => {
@@ -29,11 +73,22 @@ export default function PrayerTimesScreen() {
 
       const hijriInfo = data[day].date.hijri;
       setHijriDate(`${hijriInfo.day} ${hijriInfo.month.en} ${hijriInfo.year}`);
-
     };
 
     fetchInfo();
+
+    return () => {
+      if (countdownRef.current) {
+        clearInterval(countdownRef.current);
+      }
+    };
   }, []);
+
+  useEffect(() => {
+    if(prayerTimes) {
+      getNextPrayer();
+    }
+  }, [prayerTimes]);
 
   return (
     <View style={styles.container}>
@@ -50,6 +105,8 @@ export default function PrayerTimesScreen() {
         </View>
       ) : null}
       {hijriDate ? <Text>{hijriDate}</Text> : null}
+      {nextPrayer ? <Text>Next prayer: {nextPrayer}</Text> : null}
+      {timeToNextPrayer ? <Text>Time to next prayer: {timeToNextPrayer}</Text> : null}
       <StatusBar style="auto" />
     </View>
 );
