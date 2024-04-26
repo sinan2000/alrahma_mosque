373d6f83e193f2760bc2ed9a266be76d6db589ed
Sinan <sinan_ceviker@yahoo.com>
2024-04-26 17:31:21 +0200

    set more screen

--- screens/MoreScreen.js
+++ screens/MoreScreen.js
@@ -2,18 +2,18 @@ import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
-const Item = ( {title, description, icon, onPress} ) => {
-    return(
+const Item = ({ title, description, iconName, onPress }) => {
+    return (
+      <View>
         <TouchableOpacity style={styles.row} onPress={onPress}>
-            <Image source={icon} style={styles.icon} />
-            <View style={styles.textContainer}>
-                <Text style={styles.title}>{title}</Text>
-                <Text style={styles.description}>{description}</Text>
-            </View>
-            <AntDesign name="right" size={24} color="black" />
+          <Image source={iconName} style={styles.icon} />
+          <Text style={styles.title}>{title}</Text>
+          <AntDesign name="right" size={20} color="black" />
         </TouchableOpacity>
+        <Text style={styles.description}>{description}</Text>
+      </View>
     );
-};
+  };
 
 export default function MoreScreen({ navigation }) {
     return(
@@ -21,19 +21,19 @@ export default function MoreScreen({ navigation }) {
       <Item 
         title='99 Names of Allah SWT'
         description='Learn the beautiful names of Allah SWT'
-        icon={require('../assets/names.png')}
+        iconName={require('../assets/names.png')}
         onPress={() => navigation.navigate('NamesScreen')}
       />
       <Item 
         title='Dua and Dhikr'
         description='Learn the supplications and remembrances'
-        icon={require('../assets/dua.png')}
+        iconName={require('../assets/dua.png')}
         onPress={() => navigation.navigate('DuaScreen')}
       />
       <Item 
         title='Settings'
         description='Change the settings of the app'
-        icon={require('../assets/settings.png')}
+        iconName={require('../assets/settings.png')}
         onPress={() => navigation.navigate('SettingsScreen')}
       />
     </View>
@@ -42,34 +42,35 @@ export default function MoreScreen({ navigation }) {
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#f2f1f6',
+        flex: 1,
+        backgroundColor: '#f2f1f6',
     },
     row: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      backgroundColor: '#F7F7F7',
-      padding: 20,
-      marginTop: 20,
-      borderRadius: 10,
-      marginHorizontal: 15,
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#F7F7F7',
+        padding: 20,
+        borderRadius: 10,
+        marginHorizontal: 25,
+        marginTop: 20,
+        marginBottom: 10,
     },
     icon: {
-      width: 40,
-      height: 40,
-      resizeMode: 'contain',
-    },
-    textContainer: {
-      flex: 1,
-      paddingHorizontal: 10,
+        width: 30,
+        height: 30,
+        resizeMode: 'contain',
+        marginRight: 10, // space between the icon and the title
     },
     title: {
-      fontSize: 18,
-      fontWeight: 'bold',
+        fontSize: 18,
+        fontWeight: 'bold',
+        flex: 1, // ensures title takes up the available space between icon and arrow
     },
     description: {
-      fontSize: 14,
-      color: 'gray',
+        fontSize: 14,
+        color: 'gray',
+        paddingHorizontal: 35, // aligns horizontally with the row content
+        //paddingBottom: 20, // space between the description and next row
     },
   });
   
